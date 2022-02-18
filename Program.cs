a305f89826fb675f7a019bd2f66daaf2432c3204
BelgVolish <gleb.shilov.07@bk.ru>
2022-02-18 12:36:48 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Text;
+
+namespace HomeWorkTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number = 0;
+            Console.WriteLine("Здравствуйте, введите число!");
+            Number = Convert.ToInt32(Console.ReadLine());
+            if (Number % 2 == 0)
+            {
+                Console.WriteLine("Число четное.");
+            }
+            else
+            {
+                Console.WriteLine("Число нечетное.");
+            }
+
+        }
+    }
+}
